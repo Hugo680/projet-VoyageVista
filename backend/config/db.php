61b9f2f5d6d277d6da85284a4cb32abf7668962a
Hugo680 <hugo.poilly@edu.ece.fr>
2026-05-26 12:39:24 +0200

    Hugo data

---
+++ backend/config/db.php
@@ -0,0 +1,26 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+$host = "localhost";
+$dbname = "voyagevista";
+$username = "root";
+$password = "root";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode([
+        "success" => false,
+        "message" => "Erreur de connexion à la base de données"
+    ]);
+    exit;
+}
+?>
